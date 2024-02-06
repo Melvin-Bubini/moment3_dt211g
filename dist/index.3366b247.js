3913945b609e0a6ec9c56a3264346e499e863d90
Melvin Bubini <melvin.bubini@gmail.com>
2024-02-06 13:10:49 +0100

    dist

---
+++ dist/index.3366b247.js
@@ -0,0 +1,2 @@
+!function(){let e=document.getElementById("open-menu"),n=document.getElementById("close-menu");function t(){let e=document.getElementById("nav-menu");"none"===window.getComputedStyle(e).display?e.style.display="block":e.style.display="none"}e.addEventListener("click",t),n.addEventListener("click",t)}();
+//# sourceMappingURL=index.3366b247.js.map
---
+++ dist/index.5ec2cbed.js
@@ -0,0 +1,2 @@
+let e=document.getElementById("open-menu"),n=document.getElementById("close-menu");function t(){let e=document.getElementById("nav-menu");"none"===window.getComputedStyle(e).display?e.style.display="block":e.style.display="none"}e.addEventListener("click",t),n.addEventListener("click",t);
+//# sourceMappingURL=index.5ec2cbed.js.map
